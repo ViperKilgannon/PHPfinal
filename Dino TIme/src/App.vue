7448cf168c34c4ea69f6e01cb2a72ac25cb06197
Nigel Simms <nigel.simms@easternstudent.ca>
2020-02-12 11:59:26 -0400

    Started project layout

---
+++ Dino TIme/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-content>
+      <!-- new stuff here -->
+      <Home />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+import Admin from "./components/Admin.vue";
+import User from "./components/User.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Home
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
